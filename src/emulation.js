9a3658fd8c97940df6d0d228bf43c3ca2097d7bb
Lionel Landwerlin <llandwerlin@gmail.com>
2014-05-05 00:58:08 +0100

    runtime: make browser emulation more robust

--- src/emulation.js
+++ src/emulation.js
@@ -3,20 +3,35 @@ const GLib = imports.gi.GLib;
 let inject = function() {
     let scope = window;
     scope.setTimeout = function(cb, time) {
-        return GLib.timeout_add(GLib.PRIORITY_DEFAULT, time, function() {
+        let timeout = {
+            cleared: false,
+            id: 0
+        };
+
+        timeout.id = GLib.timeout_add(GLib.PRIORITY_DEFAULT, time, function() {
             cb();
+            timeout.cleared = true;
             return false;
         }, null, null);
+        return timeout;
     };
     scope.setInterval = function(cb, time) {
-        return GLib.timeout_add(GLib.PRIORITY_DEFAULT, time, function() {
-            cb();
-            return true;
-        }, null, null);
+        let timeout = {
+            cleared: false,
+            id: GLib.timeout_add(GLib.PRIORITY_DEFAULT, time, function() {
+                cb();
+                return true;
+            }, null, null)
+        };
+        return timeout;
     };
-    scope.clearTimeout = function(id) {
-        if (id > 0)
-            GLib.source_remove(id);
+    scope.clearTimeout = function(timeout) {
+        if (timeout !== undefined &&
+            timeout !== null &&
+            timeout.id > 0 &&
+            !timeout.cleared) {
+            GLib.source_remove(timeout.id);
+        }
     };
     scope.clearInterval = scope.clearTimeout;
 };
