cbfe4cb3863c4ce0cc213a2acbe79161e8e73982
Lionel Landwerlin <llandwerlin@gmail.com>
2014-07-15 01:19:20 +0100

    runtime: remove unused runtimeMain file

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="noflo-gnome"
 
-(test -f $srcdir/src/runtimeMain.js) || {
+(test -f $srcdir/src/main.js) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
--- src/runtimeMain.js
+++
@@ -1,7 +0,0 @@
-const WebProtoServer = imports.websocketServer;
-const Runtime = imports.runtime;
-
-let start = function() {
-    WebProtoServer.getDefault().start();
-    Runtime.start();
-};
