c43259909ca0e06b90bc2f0c2c093afaf3041051
Lionel Landwerlin <llandwerlin@gmail.com>
2014-07-26 14:24:27 +0100

    runtime: fix graph autosave with new path infrastructure

--- src/codeWriter.js
+++ src/codeWriter.js
@@ -1,9 +1,10 @@
 const GLib = imports.gi.GLib;
 const Path = imports.path;
+const Runtime = imports.runtime;
 const Utils = imports.utils;
 
 let writeGraph = function(graph) {
-    let graphPath = Path.CURRENT_DIR + '/graphs/' + graph.name + '.json';
+    let graphPath = Runtime.resolvePath('local://graphs/' + graph.name + '.json');
 
     log('writing ' + graphPath);
 
